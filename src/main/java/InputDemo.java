a2a53eda7dc5c40ffe1823e5c7e84ef7cfcbcf90
Daniel Szlicht <daniel.szlicht25@gmail.com>
2025-09-22 14:13:20 +0200

    input

---
+++ src/main/java/InputDemo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class InputDemo {
+    public static void main(String[] args) {
+        //tworzenie obiektu
+        //Typ nazwa = new Typ();
+
+        Scanner input = new Scanner(System.in);
+        //next,nextLine,nextInt, nextDouble, nextBoolean
+        System.out.println("Jak się nazywasz?");
+        String name = input.nextLine();
+        System.out.println("Jaka jest Twoja ulubiona liczba?");
+        int number = input.nextInt();
+        int multiplication = number*2;
+        System.out.println("Cześć " + name + " Twoja liczba *2 to:"+
+        multiplication);
+    }
+}
